0bf16ee20f95d1e9a6e9949088fe0d7dd341a72c
nag-murali <93373775+nag-murali@users.noreply.github.com>
2022-01-23 22:24:50 +0530

    update

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Address = require("../models/address.model.js")
-const authenticate = require("../middlewares/authenticate");
+
 
 
 
