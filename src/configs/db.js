6c719d4e57b942606c730b5f7c47b4e2812b8089
Nagendra Muralidhar Pulla <93373775+nag-murali@users.noreply.github.com>
2022-03-09 11:59:25 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-    return mongoose.connect("mongodb+srv://murali:murali@web-test.6swdq.mongodb.net/zarahomeproject?retryWrites=true&w=majority");
+    return mongoose.connect(process.env.mongoUrl);
 }
